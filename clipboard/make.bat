daa00897968967571ea68e9da171e45c2ae74f49
ahoeben <aldo@fieldofview.com>
2011-03-06 16:19:12 +0100

    add clipboard, dblclick, vectormath and string plugins

---
+++ clipboard/make.bat
@@ -0,0 +1,2 @@
+mxmlc -target-player=10.0.0 -use-network=false -static-link-runtime-shared-libraries=true -output=..\clipboard.swf source\clipboard.as
+pause
---
+++ dblclick/make.bat
@@ -0,0 +1,2 @@
+mxmlc -target-player=10.0.0 -use-network=false -static-link-runtime-shared-libraries=true -output=..\dblclick.swf source\dblclick.as
+pause
