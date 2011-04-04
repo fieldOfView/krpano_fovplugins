d94cc7af2ed6c27d8feb5ea17d7245d8c754a6fb
ahoeben <aldo@fieldofview.com>
2011-04-04 14:14:36 +0200

    Added a js version of the dblclick plugin for krpanoJS

---
+++ dblclick.js
@@ -0,0 +1,10 @@
+/*
+	krpano HTML5 dblclick plugin
+	by Aldo Hoeben / fieldofview.com
+		
+	http://fieldofview.github.com/krpano_fovplugins/dblclick/plugin.html
+	This software can be used free of charge and the source code is available under a Creative Commons Attribution license:
+		http://creativecommons.org/licenses/by/3.0/		
+*/
+
+var krpanoplugin=function(){var b=this,d=null,c=null,e;b.registerplugin=function(g,f,h){d=g;c=h;if(d.version<"1.0.8.14"||d.build<"2011-03-30"){d.trace(3,"dblclick plugin - too old krpano version (min. 1.0.8.14)");return}c.registerattribute("ondblclick","",function(i){e=i},function(){return e});d.control.layer.addEventListener("dblclick",a,true)};b.unloadplugin=function(){d.control.layer.removeEventListener("dblclick",a);c=null;d=null};function a(f){d.call(e)}};
